60e48d68c18c6eac4ea2b548f2f0c7b69e96ab7a
ponce <aliloko@gmail.com>
2014-07-06 16:29:33 +0200

    Custom exception now follow "D Cookbook" guidelines.

--- source/y4md/y4m.d
+++ source/y4md/y4m.d
@@ -15,9 +15,9 @@ class Y4MException : Exception
 {
     public
     {
-        this(string msg)
+        this(string message, string file =__FILE__, size_t line = __LINE__, Throwable next = null)
         {
-            super(msg);
+            super(message, file, line, next);
         }
     }
 }
